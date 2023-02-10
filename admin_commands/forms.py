7d8bb60b0d5f20c57bae4c7329857a004f1a14de
Ramez <ramez@kuwaitnet.com>
2023-02-10 21:45:44 +0200

    Translations + fixes

--- admin_commands/forms.py
+++ admin_commands/forms.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 
 class ExecuteCommandForm(forms.Form):
     # command = forms.ModelChoiceField(ManagementCommand.objects.all())
-    args = forms.CharField(required=False, label=_('Arguments'), help_text=_('Arguments for command'))
+    args = forms.CharField(required=False, label=_('Arguments to send to command'))
