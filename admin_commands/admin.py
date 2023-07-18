4be5ce5c846fbf46f8a06da8491d6d3f5d230a22
Ramez Ashraf <ramez@kuwaitnet.com>
2023-07-18 11:21:18 +0200

    v.1.1.2 Add Django 4.1+ support

--- admin_commands/admin.py
+++ admin_commands/admin.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.template.defaultfilters import linebreaksbr
 from django.urls import reverse
-from django.utils.encoding import force_text
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
@@ -89,7 +88,7 @@ class CommandAdminBase(admin.ModelAdmin):
         opts = self.model._meta
         context = dict(
             self.admin_site.each_context(request),
-            module_name=force_text(opts.verbose_name_plural),
+            module_name=str(opts.verbose_name_plural),
             has_add_permission=self.has_add_permission(request),
             opts=opts,
             command=command,
